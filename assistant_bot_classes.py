d8237ccc493c69946197ce590721a8024abc98ce
kravchenmd <66253910+kravchenmd@users.noreply.github.com>
2022-05-30 23:29:37 +0200

    Initial change of the assistant_bot code to fit the description of the HW 9

--- assistant_bot_classes.py
+++ assistant_bot_classes.py
@@ -1,5 +1,5 @@
-exit_commands = ('good bye', 'close', 'exit')
-contacts = {}
+EXIT_COMMANDS = ('good bye', 'close', 'exit')
+CONTACTS = {}
 
 
 # This decorator handles correctness of the phone number: can start with '+'
@@ -17,9 +17,8 @@ def input_error(func):
             if int(phone_check) < 0:
                 raise ValueError
         except ValueError:
-            print("ERROR: Phone can or couldn't start with '+' and then must contain only digits!")
-            print("Example: +380..., 380...")
-            return
+            return "ERROR: Phone can or couldn't start with '+' and then must contain only digits!\n \
+                    Example: +380..., 380..."
 
         result = func(name, phone)
         return result
@@ -31,67 +30,70 @@ def input_error(func):
 def func_arg_error(func):
     def wrapper(*args):
         try:
-            func(*args)
+            result = func(*args)
+            return result
         except TypeError:
             f_name = func.__name__
             if f_name in ('hello', 'show_all_phones'):
-                print("ERROR: This command has to be written without arguments!")
+                return "ERROR: This command has to be written without arguments!"
             if f_name in ('add_phone', 'change_phone'):
-                print("ERROR: This command needs 2 arguments: 'name' and 'phone' separated by 1 space!")
+                return "ERROR: This command needs 2 arguments: 'name' and 'phone' separated by 1 space!"
             if f_name == 'show_phone':
-                print("ERROR: This command needs 1 arguments: 'name' separated by 1 space!")
+                return "ERROR: This command needs 1 arguments: 'name' separated by 1 space!"
 
     return wrapper
 
 
 @func_arg_error
-def hello() -> None:
-    print("Hello! How can I help you?")
+def hello() -> str:
+    return "Hello! How can I help you?"
 
 
 @input_error
 @func_arg_error
-def add_phone(name: str, phone: str) -> None:
-    if name in contacts.keys():
-        print(f"Name '{name}' is already in contacts!")
-        print("Try another name or change existing contact")
-        return
+def add_phone(name: str, phone: str) -> str:
+    if name in CONTACTS.keys():
+        return f"Name '{name}' is already in contacts!\n \
+                Try another name or change existing contact"
 
-    contacts.update({name: phone})
-    print(f'Contact was added successfully!')
+    CONTACTS.update({name: phone})
+    return f"Contact was added successfully!"
 
 
 @input_error
 @func_arg_error
-def change_phone(name: str, phone: str):
-    if name not in contacts.keys():
-        print(f"There is no contact with name '{name}'")
-        return
+def change_phone(name: str, phone: str) -> str:
+    if name not in CONTACTS.keys():
+        return f"There is no contact with name '{name}'"
 
-    contacts.update({name: phone})
-    print(f'Contact was updated successfully!')
-
-
-@func_arg_error
-def show_phone(name: str) -> None:
-    if name not in contacts.keys():
-        print(f"There is no contact with name '{name}'")
-        return
-
-    print(contacts.get(name))
+    CONTACTS.update({name: phone})
+    return f"Contact was updated successfully!"
 
 
 @func_arg_error
-def show_all_phones() -> None:
-    if not contacts:
-        print("There are no contacts to show yet...")
-        return
+def show_phone(name: str) -> str:
+    if name not in CONTACTS.keys():
+        return f"There is no contact with name '{name}'"
 
-    for name, phone in contacts.items():
-        print(name, phone)
+    return CONTACTS.get(name)
+
+
+@func_arg_error
+def show_all_phones() -> str:
+    if not CONTACTS:
+        return "There are no contacts to show yet..."
+
+    return '\n'.join(["{:<10}: {:<10}".format(name, phone) for name, phone in CONTACTS.items()])
+
+
+def exit_program():
+    return "Good bye!"
 
 
 def choose_command(cmd: list):
+    if cmd in EXIT_COMMANDS:
+        return exit_program
+
     cmd_check = cmd[0].lower()
 
     if cmd_check == 'hello':
@@ -109,15 +111,13 @@ def choose_command(cmd: list):
         if cmd_check == 'show all':
             return show_all_phones
 
-    print('Unknown command')
-    return None
 
-
-def handle_command(cmd: str):
+def parse_command(cmd: str):
     cmd = cmd.strip().split(' ')  # apply strip() as well to exclude spaces at the ends
-    func = choose_command(cmd)
+    func, result = choose_command(cmd), "Unknown command!"  # default result
     if func:
-        func(*cmd[1:]) if len(cmd) > 1 else func()  # else part to take into account hello() and show()
+        result = func(*cmd[1:]) if len(cmd) > 1 else func()  # else part to take into account hello() and show()
+    return func, result
 
 
 def main():
@@ -128,11 +128,16 @@ def main():
         while not command:
             command = input('Enter command: ')
 
-        if command in exit_commands:
+        if command in EXIT_COMMANDS:
             print("Good bye!")
             break
 
-        handle_command(command)
+        func, result = parse_command(command)
+
+        print(result)
+
+        if func == exit_program:
+            break
         print()
 
 
